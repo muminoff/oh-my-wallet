e059b6dca9be8a2999ecdbe1fed10ceb5822f720
sardor <sardor@hanbiro.com>
2014-08-29 20:10:38 +0900

    Menu change

--- core/views.py
+++ core/views.py
@@ -5,6 +5,7 @@ from core.models import Category, Expense
 
 def all_expenses(request):
     context = {
-        'all_expenses': Expense.objects.all()
+        'all_expenses': Expense.objects.all(),
+        'all_categories': Category.objects.all()
     }
     return render(request, 'all_expenses.html', context)
